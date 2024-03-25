8e57945a3bf51ad955f72e74be25778c8ce7df48
Laiba6 <laiba.ahmed@dcmail.ca>
2024-03-25 00:07:44 -0400

    added the looping

--- assignment4/part2/gallery-start/gallery-start/main.js
+++ assignment4/part2/gallery-start/gallery-start/main.js
@@ -13,19 +13,25 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 const images = ["pic1.jpg","pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg" ]
+
+/* Declaring the alternative text for each image file */
 const alts = {"pic1.jpg" : "Closeup of a human eye", 
               "pic2.jpg" : "A wave rock",
               "pic3.jpg" : "Purple & white flowers",
               "pic4.jpg" : "An acient wall",
               "pic5.jpg" : "A moth on a leaf" }
 
-/* Declaring the alternative text for each image file */
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
+for (const image of images)
+{const newImage = document.createElement('img');
+newImage.setAttribute('src', "images/${image}");
+newImage.setAttribute('alt', [image]);
 thumbBar.appendChild(newImage);
+newImage.addEventListener("click", e => {
+displayedImage.src = e.target.src;
+displayedImage.alt = e.target.alt;});
+}
+
 
 /* Wiring up the Darken/Lighten button */
