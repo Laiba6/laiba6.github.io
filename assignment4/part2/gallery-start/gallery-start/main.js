e26216c39d5be0b7a43de28c03b081438cdde811
Laiba6 <laiba.ahmed@dcmail.ca>
2024-04-01 00:24:00 -0400

    added html comments

--- assignment4/part2/gallery-start/gallery-start/main.js
+++ assignment4/part2/gallery-start/gallery-start/main.js
@@ -1,9 +1,8 @@
 /*
-Name;
-Date:
-Filename:
+Name: Laiba Ahmed
+Date: March 25, 2024
+Filename: Main.js
 Description: Image Gallery
-
 */
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
@@ -12,26 +11,40 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const images = ["pic1.jpg","pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg" ]
+const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
 
 /* Declaring the alternative text for each image file */
-const alts = {"pic1.jpg" : "Closeup of a human eye", 
-              "pic2.jpg" : "A wave rock",
-              "pic3.jpg" : "Purple & white flowers",
-              "pic4.jpg" : "An acient wall",
-              "pic5.jpg" : "A moth on a leaf" }
-
-
-/* Looping through images */
-for (const image of images)
-{const newImage = document.createElement('img');
-newImage.setAttribute('src', "images/${image}");
-newImage.setAttribute('alt', [image]);
-thumbBar.appendChild(newImage);
-newImage.addEventListener("click", e => {
-displayedImage.src = e.target.src;
-displayedImage.alt = e.target.alt;});
+const alts = {
+  'pic1.jpg' : 'Closeup of a human eye',
+  'pic2.jpg' : 'A painting of a wave',
+  'pic3.jpg' : 'Purple and white flowers',
+  'pic4.jpg' : 'A ancient wall',
+  'pic5.jpg' : 'A moth on a leaf'
 }
 
+/* Looping through images */
+for (const image of images) {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', `images/${image}`);
+  newImage.setAttribute('alt', alts[image]);
+  thumbBar.appendChild(newImage);
+  newImage.addEventListener('click', e => {
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+  });
+}
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', () => {
+  const btnClass = btn.getAttribute('class');
+  if (btnClass === 'dark') {
+    btn.setAttribute('class','light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+});
+
